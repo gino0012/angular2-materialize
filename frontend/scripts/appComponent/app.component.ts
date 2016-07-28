901db7adabbee4a7ecb1f1ed96c14e5e2a1b1140
Gino Carlo Saavedra <gino.saavedra@nokia.com>
2016-07-28 16:51:44 +0800

    add materialize css

--- frontend/scripts/appComponent/app.component.ts
+++ frontend/scripts/appComponent/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavComponent} from '../navComponent/nav.component';
+import {FooterComponent} from "../footerComponent/footer.component";
+import {MainComponent} from "../mainComponent/main.component";
+
 @Component({
-  selector: 'my-app',
-  templateUrl: 'scripts/appComponent/app.component.html'
+  selector: 'app',
+  templateUrl: 'scripts/appComponent/app.component.html',
+  directives: [NavComponent, FooterComponent, MainComponent]
 })
 export class AppComponent { }
---
+++ frontend/scripts/navComponent/nav.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: 'scripts/navComponent/nav.component.html'
+})
+export class NavComponent { }
